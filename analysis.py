12036035f195b3d7cb24fdd049fb0e84a0e03714
cerubins <cerubins@umich.edu>
2023-04-09 11:00:57 -0400

    bert complete + analysis step
    Co-authored-by: Ranya Liu <ranyal507@users.noreply.github.com>

--- analysis.py
+++ analysis.py
@@ -1,4 +1,18 @@
+import glob
 
+toxicScores = dict()
 for file in glob.glob('output/*'):
     with open(file) as f:
-        # meh
+        subreddit = file.lstrip('output/')
+        lines = file.readlines()
+
+        toxicCount = 0
+        for line in lines:
+            words = line.split()
+            if words[-1] == 'Toxic':
+                toxicCount += 1
+
+        toxicScores[subreddit] = toxicCount/len(lines)
+
+sortedScores = sorted(toxicScores)
+print(sortedScores)
