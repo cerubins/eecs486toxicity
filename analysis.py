d831714703166a1555702605cc2452c9191ffe62
Ranya Liu <97542799+ranyal507@users.noreply.github.com>
2023-04-10 16:49:54 -0400

    Add files via upload

--- analysis.py
+++ analysis.py
@@ -1,18 +1,22 @@
 import glob
 
+# iterates through all subreddit BERT predictions
 toxicScores = dict()
 for file in glob.glob('output/*'):
     with open(file) as f:
         subreddit = file.lstrip('output/')
         lines = file.readlines()
 
+        # counts number of "toxic" labeled comments in identifier
+        lineCount = 0
         toxicCount = 0
         for line in lines:
-            words = line.split()
-            if words[-1] == 'Toxic':
+            lineCount += 1
+            if lineCount % 2 == 1 and line == 'Toxic':
                 toxicCount += 1
 
         toxicScores[subreddit] = toxicCount/len(lines)
 
+# outputs subreddits sorted by toxicity percentage
 sortedScores = sorted(toxicScores)
 print(sortedScores)
