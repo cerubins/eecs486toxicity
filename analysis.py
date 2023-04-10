20975351da116f91a33dc07ca74b4251f165d653
Ranya Liu <97542799+ranyal507@users.noreply.github.com>
2023-04-10 17:06:40 -0400

    Add files via upload

--- analysis.py
+++ analysis.py
@@ -5,18 +5,19 @@ toxicScores = dict()
 for file in glob.glob('output/*'):
     with open(file) as f:
         subreddit = file.lstrip('output/')
-        lines = file.readlines()
+        lines = f.readlines()
 
         # counts number of "toxic" labeled comments in identifier
         lineCount = 0
         toxicCount = 0
         for line in lines:
             lineCount += 1
-            if lineCount % 2 == 1 and line == 'Toxic':
+            if lineCount % 2 == 0 and 'Non' not in line:
                 toxicCount += 1
 
         toxicScores[subreddit] = toxicCount/len(lines)
 
 # outputs subreddits sorted by toxicity percentage
 sortedScores = sorted(toxicScores)
-print(sortedScores)
+for subreddit in sortedScores:
+    print(subreddit, " ", toxicScores[subreddit], "\n")
